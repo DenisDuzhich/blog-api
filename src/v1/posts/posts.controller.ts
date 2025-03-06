98cb83e5e2d0ab1efb69575c145aaec6d83de96b
Denis Duzhich <mnogotcht@gmail.com>
2025-03-06 08:57:13 +0200

    Revert "Merge branch 'auth-implementation'"
    This reverts commit abba1472a7291b05adf34d65f4b583d355b1830a, reversing
    changes made to e192e1a922d39978839cd18bf0c0de56aeff68e2.

--- src/v1/posts/posts.controller.ts
+++ src/v1/posts/posts.controller.ts
@@ -6,12 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostEntity } from './entities/post.entity';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @ApiTags('V1/Posts')
 @Controller('posts')
@@ -19,12 +21,15 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   @ApiCreatedResponse({ type: PostEntity })
   async create(@Body() createPostDto: CreatePostDto) {
     return new PostEntity(await this.postsService.create(createPostDto));
   }
 
   @Get()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: PostEntity, isArray: true })
   async findAll() {
     const posts = await this.postsService.findAll();
@@ -32,18 +37,24 @@ export class PostsController {
   }
 
   @Get(':id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: PostEntity })
   async findOne(@Param('id') id: string) {
     return new PostEntity(<PostEntity>await this.postsService.findOne(id));
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: PostEntity })
   async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return new PostEntity(await this.postsService.update(id, updatePostDto));
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: PostEntity })
   async remove(@Param('id') id: string) {
     return new PostEntity(await this.postsService.remove(id));
